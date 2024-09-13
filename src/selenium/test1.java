01bf691ae368392a163b651352c4321061aa1587
Arpitasharath <arpitasharath@gmail.com>
2024-09-13 20:41:04 +0530

    push one

---
+++ src/selenium/test1.java
@@ -0,0 +1,12 @@
+package selenium;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
